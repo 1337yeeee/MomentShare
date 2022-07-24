566cb04c66f4db968658f07c1b8eb8ca7202dd5b
Anatoly <none>
2022-07-24 12:31:08 +0300

    updated Message: and message and callback_query

--- Message.py
+++ Message.py
@@ -2,11 +2,14 @@
 
 class Message:
 
-	def __init__(self, message):
-		self.message = message
-		self.name = message['chat']['first_name']
-		self.chat_id = message['chat']['id']
-		self.username = message['from']['username']
+	def __init__(self, update):
+		self.message_id = None
+		self.data = None
+		self.callback = None
+		self.message = None
+		self.name = None
+		self.chat_id = None
+		self.username = None
 		self.text = ''
 		self.is_command = False
 		self.is_mention = False
@@ -17,30 +20,64 @@ class Message:
 		self.hashtag = ''
 		self.photo_id = ''
 
-		try: self.text = message['text']
-		except KeyError: self.text = None
+		update_type = ''
+		try:
+			if update['message']:
+				update_type = 'message'
+		except KeyError:
+			try:
+				if update['callback_query']:
+					update_type = 'callback'
+			except KeyError as e:
+				print('an error occurred in Message constructor\n', e)
+				print(update)
+
+		if update_type == 'message':
+			self.message_constructor(update)
+		elif update_type == 'callback':
+			self.callback_constructor(update)
+
+	def message_constructor(self, update):
+		self.message = update['message']
+		self.name = self.message['chat']['first_name']
+		self.chat_id = self.message['chat']['id']
+		self.username = self.message['from']['username']
+		self.message_id = self.message['message_id']
 
 		try:
-			if message['photo']:
+			self.text = self.message['text']
+		except KeyError:
+			self.text = None
+
+		try:
+			if self.message['photo']:
 				self.is_photo = True
-				self.photo_id = message['photo'][0][3]['file_id']
+				self.photo_id = self.message['photo'][0][3]['file_id']
 		except KeyError:
 			pass
 
 		try:
-			for i in range(len(message['entities'])):
-				if message['entities'][i]['type'] == 'bot_command':
+			for i in range(len(self.message['entities'])):
+				if self.message['entities'][i]['type'] == 'bot_command':
 					self.is_command = True
 					self.command = self.get_from_text(i)
-					
-				elif message['entities'][i]['type'] == 'mention':
+
+				elif self.message['entities'][i]['type'] == 'mention':
 					self.is_mention = True
 					self.mention = self.get_from_text(i).replace('@', '')
-				elif message['entities'][i]['type'] == 'hashtag':
+				elif self.message['entities'][i]['type'] == 'hashtag':
 					self.is_hashtag = True
 					self.hashtag = self.get_from_text(i)
 		except KeyError:
 			pass
+
+	def callback_constructor(self, update):
+		self.callback = update['callback_query']
+		self.data = self.callback['data']
+		self.chat_id = self.callback['from']['id']
+		self.username = self.callback['from']['username']
+		self.message_id = self.callback['message']['message_id']
+		self.text = self.callback['message']['text']
 
 	def get_from_text(self, index):
 		offset = self.message['entities'][index]['offset']
